9fa0a8281d70f258fc1ef14a86ab2847e2903ada
Cyril-Meyer <contact@cyrilmeyer.eu>
2025-01-30 17:27:58 +0100

    finalize new database format (remove older one)

--- database.py
+++ database.py
@@ -8,14 +8,11 @@ class EmailDB:
     def __init__(self,
                  db_filename='mail.db',
                  attachments_filename='mail_attach.zip',
-                 init=True,
-                 single_table_alternative=True):
+                 init=True):
         if not db_filename.endswith('.db'):
             db_filename = db_filename + '.db'
         if not attachments_filename.endswith('.zip'):
             attachments_filename = attachments_filename + '.zip'
-        # Alternative DB
-        self.single_table_alternative = single_table_alternative
         # SQLite DB
         self.conn = sqlite3.connect(db_filename)
         self.cursor = self.conn.cursor()
@@ -25,86 +22,44 @@ class EmailDB:
         self.zip = zipfile.ZipFile(attachments_filename, 'w', zipfile.ZIP_DEFLATED)
 
     def init_database(self):
-        if self.single_table_alternative:
-            self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS emails (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                subject TEXT,
-                sender TEXT,
-                date TIMESTAMP,
-                body TEXT,
-                attachments TEXT
-            )
-            ''')
-        else:
-            self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS emails (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                subject TEXT,
-                sender TEXT,
-                date TIMESTAMP,
-                body TEXT
-            )
-            ''')
-
-            self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS attachments (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                email_id INTEGER NOT NULL,
-                file_name TEXT NOT NULL,
-                file_hash TEXT NOT NULL,
-                FOREIGN KEY (email_id) REFERENCES emails (id)
-            )
-            ''')
-
-        self.conn.commit()
-
-    def add_email(self, subject, sender, date, body):
         self.cursor.execute('''
-        INSERT INTO emails (subject, sender, date, body)
-        VALUES (?, ?, ?, ?)
-        ''', (subject, sender, date, body))
-
-        self.conn.commit()
-        return self.cursor.lastrowid
-
-    def add_attachment(self, email_id, file_name, file_data):
-        file_hash = hashlib.sha256(file_data).hexdigest()
-        self.cursor.execute('''
-        INSERT INTO attachments (email_id, file_name, file_hash)
-        VALUES (?, ?, ?)
-        ''', (email_id, file_name, file_hash))
-
-        file_out = file_hash + pathlib.Path(file_name).suffix
-        if not file_out in self.zip.namelist():
-            self.zip.writestr(file_out, file_data)
+        CREATE TABLE IF NOT EXISTS emails (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject TEXT,
+            sender TEXT,
+            date TIMESTAMP,
+            body TEXT,
+            attachments TEXT
+        )
+        ''')
 
         self.conn.commit()
 
-    def add_email_and_attachment(self, subject, sender, date, body, attach):
-        attach_info = ''
-        for file_name, file_data in attach:
+    def add_attachment(self, attachments):
+        attachments_info = ''
+
+        for file_name, file_data in attachments:
             file_hash = hashlib.sha256(file_data).hexdigest()
             file_out = file_hash + pathlib.Path(file_name).suffix
+            attachments_info += f'{file_name}<=>{file_out}\n'
+            # Check that file is not duplicate
             if not file_out in self.zip.namelist():
                 self.zip.writestr(file_out, file_data)
-            attach_info += f'{file_name}<=>{file_out}\n'
 
+        return attachments_info
+
+    def add_email(self, subject, sender, date, body, attachments_info):
         self.cursor.execute('''
         INSERT INTO emails (subject, sender, date, body, attachments)
         VALUES (?, ?, ?, ?, ?)
-        ''', (subject, sender, date, body, attach_info))
+        ''', (subject, sender, date, body, attachments_info))
 
         self.conn.commit()
 
     def add(self, email):
         mail_subject, mail_sender, mail_date, mail_body, mail_attach = email
-        if self.single_table_alternative:
-            self.add_email_and_attachment(mail_subject, mail_sender, mail_date, mail_body, mail_attach)
-        else:
-            email_id = self.add_email(mail_subject, mail_sender, mail_date, mail_body)
-            for file_name, file_data in mail_attach:
-                self.add_attachment(email_id, file_name, file_data)
+        attachments_info = self.add_attachment(mail_attach)
+        self.add_email(mail_subject, mail_sender, mail_date, mail_body, attachments_info)
 
     def close(self):
         self.conn.close()
