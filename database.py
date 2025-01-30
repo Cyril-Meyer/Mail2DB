69a46cce37fc61e96dafa51c1a529cb781f69b40
Cyril-Meyer <contact@cyrilmeyer.eu>
2025-01-30 16:34:55 +0100

    add database module to handle sqlite data

---
+++ database.py
@@ -0,0 +1,63 @@
+import datetime
+import hashlib
+import sqlite3
+
+
+class EmailDB:
+    def __init__(self, filename='mail.db', init=True):
+        self.conn = sqlite3.connect(filename)
+        self.cursor = self.conn.cursor()
+        if init:
+            self.init_database()
+
+    def init_database(self):
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS emails (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject TEXT,
+            sender TEXT,
+            date TIMESTAMP,
+            body TEXT
+        )
+        ''')
+
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS attachments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email_id INTEGER NOT NULL,
+            file_name TEXT NOT NULL,
+            file_hash TEXT NOT NULL,
+            FOREIGN KEY (email_id) REFERENCES emails (id)
+        )
+        ''')
+
+        self.conn.commit()
+
+    def add_email(self, subject, sender, date, body):
+        self.cursor.execute('''
+        INSERT INTO emails (subject, sender, date, body)
+        VALUES (?, ?, ?, ?)
+        ''', (subject, sender, date, body))
+
+        self.conn.commit()
+        return self.cursor.lastrowid
+
+    def add_attachment(self, email_id, file_name, file_data):
+        file_hash = hashlib.sha256(file_data).hexdigest()
+        self.cursor.execute('''
+        INSERT INTO attachments (email_id, file_name, file_hash)
+        VALUES (?, ?, ?)
+        ''', (email_id, file_name, file_hash))
+
+        self.conn.commit()
+
+    def add(self, email):
+        mail_subject, mail_sender, mail_date, mail_body, mail_attach = email
+        email_id = self.add_email(mail_subject, mail_sender, mail_date, mail_body)
+        for file_name, file_data in mail_attach:
+            self.add_attachment(email_id, file_name, file_data)
+
+    def close(self):
+        self.conn.close()
+
+
